a6ccf2d25df982521e683efdd62d4f1155537041
robert erickson <bobericksonjr@gmail.com>
2021-01-04 11:41:55 -0500

    models

--- cooking/admin.py
+++ cooking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, RecipeItem, Complement, Menu
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(RecipeItem)
+admin.site.register(Complement)
+admin.site.register(Menu)
--- cooking/models.py
+++ cooking/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-import djfractions
 
 
 # Create your models here.
@@ -8,11 +7,24 @@ class User(AbstractUser):
     pass
 
 
-class RecipeItems(models.Model):
-    name = models.CharField(max_lenth=100)
-    
+class RecipeItem(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(blank=True)
+    prep_time = models.IntegerField()
+    cook_time = models.IntegerField()
+    servings = models.IntegerField()
+    ingredients = models.TextField()
+    directions = models.TextField()
 
-class Compliments(models.Model):
+
+class Complement(models.Model):
+    recipe_a = models.ForeignKey(RecipeItem, on_delete=models.CASCADE, related_name="complemented")
+    recipe_b = models.ForeignKey(RecipeItem, on_delete=models.CASCADE, related_name="complementee")
 
 
-class Menus(models.Model):
+class Menu(models.Model):
+    starter = models.ForeignKey(RecipeItem, on_delete=models.SET_NULL, blank=True, null=True, related_name="starter_recipe")
+    first = models.ForeignKey(RecipeItem, on_delete=models.SET_NULL, blank=True, null=True, related_name="first_recipe")
+    second = models.ForeignKey(RecipeItem, on_delete=models.SET_NULL, blank=True, null=True, related_name="second_recipe")
+    veg = models.ForeignKey(RecipeItem, on_delete=models.SET_NULL, blank=True, null=True, related_name="veg_recipe")
+    dessert = models.ForeignKey(RecipeItem, on_delete=models.SET_NULL, blank=True, null=True, related_name="dessert_recipe")
