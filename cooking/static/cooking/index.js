f49dcc3d74301ea0aea4d138bfcde50410b7f21a
robert erickson <bobericksonjr@gmail.com>
2021-01-17 16:22:46 -0500

    started JS

--- cooking/static/cooking/index.js
+++ cooking/static/cooking/index.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // for each button, when clicked, pass it to expand function
+    document.querySelectorAll('button').forEach(function(button) {
+        button.onclick = function() {
+            expand_recipe(button);
+        }
+    });
 
 });
+
+function expand_recipe(button) {
+    // get the parent element of clicked button (the recipe box)
+    recipe = button.parentElement;
+    // change direction CSS to display it
+    directions = recipe.getElementsByClassName("recipe-directions")[0];
+    directions.style.display = 'block';
+    
+    console.log(directions);
+
+    // TO DO: make read more button dissapear (display: none;)
+    // have a read less button at bottom instead (hardcoded but hidden?)
+    // when read less clicked, directions box display:none and read more diplay: block;
+
+    // SIMPLE!!
+}
